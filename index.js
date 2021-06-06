3f892ea396ef27095b439b439a6b8d7d6fe61fe0
Sushrut Kasture <kasturesushrut@gmail.com>
2021-06-06 14:15:59 +0530

    Modified files to get svg corresponding to characters

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-module.exports = require('./lib');
+let captchacreator = require('./lib');
+let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+let obj = {};
+// captchacreator.create(chars[0]);
+
+for(let i=0; i<chars.length; i++){
+    obj[chars[i]] = captchacreator.create(chars[i]).data;
+}
+let ans = JSON.stringify(obj);
+console.log(ans);
+
+const fs = require('fs')
+
+
+
+try {
+  const data = fs.writeFileSync('texts.json', ans)
+  //file written successfully
+} catch (err) {
+  console.error(err)
+}
+
+// let a = captchacreator.create("A");
+// console.log(a);
